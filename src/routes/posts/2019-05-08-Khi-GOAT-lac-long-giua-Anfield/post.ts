d80db791aa489f60c7143b3799a5b2d1528f70d1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-19 16:46:30 +0000

    Deploy khanghldk/react-blog to github.com/khanghldk/react-blog.git:master

--- src/routes/posts/2019-05-08-Khi-GOAT-lac-long-giua-Anfield/post.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  title: `Khi GOAT lạc lõng giữa Anfield`,
-  tags: ['football', 'messi'],
-  spoiler: 'Chưa ai có đủ sức tưởng tượng để nghĩ đến viễn cảnh kinh hoàng đó của FC Barcelona tại Anfield',
-  getContent: () => import('./document.mdx'),
-}
