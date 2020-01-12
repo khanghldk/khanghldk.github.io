a152542a9588ea3df83c3afc01e48609c63e8232
Khang Ha <v.khanghld@vinid.net>
2020-01-12 22:47:46 +0700

    update setMetadata

--- src/siteMetadata.js
+++ src/siteMetadata.js
@@ -1,10 +1,10 @@
 export default {
   // The blog's title as it appears in the layout header, and in the document
   // <title> tag.
-  title: "npx create-react-blog",
+  title: "Kelvin's",
 
-  author: "James K Nelson",
-  description: "A clone of Dan Abramov's overreacted.io using create-react-app-mdx and Navi",
+  author: "Kelvin Ha",
+  description: "A personal blog about technology, life and football",
 
   // The number of posts to a page on the site index.
   indexPageSize: 10,
